41dd57da823904a0b5401da3c259a427393bf8e1
Your Name <you@example.com>
2025-07-16 00:11:48 +0100

    debug

--- public/homes.js
+++ public/homes.js
@@ -38,7 +38,7 @@ async function submitPrayer(e) {
     const res = await fetch(`${baseUrl}/api/prayer`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, email, request }) // ✅ FIXED!
+      body: JSON.stringify({ name, email, message }) //FIXED!
     });
 
     const data = await res.json();
